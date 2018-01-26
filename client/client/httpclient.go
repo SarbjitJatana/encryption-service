36b36871e08ac983c9867863560195e00f830167
SARBJIT JATANA <sarbjit@SARBJITs-MacBook-Air.local>
2018-01-26 14:06:33 +0000

    Code for Storing and Retrieving data from the server. Added some structures for sending JSON data to and from the server.

--- client/client/httpclient.go
+++ client/client/httpclient.go
@@ -1,7 +1,41 @@
 package client
 
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+)
+
+const (
+	storePath    = "/store"
+	retrievePath = "/retrieve"
+)
+
+// HTTPClient is a wrapper containing the server URL
 type HTTPClient struct {
 	ServerURL string
+}
+
+// EncryptRequest defines the data that will be sent to the server for storing
+type EncryptRequest struct {
+	ID        string
+	PlainText string
+}
+
+// EncryptDecryptData contains the key used for encryption
+// The key is sent back from the server on a 'Store' request and
+// sent to the server on a 'Retrieve' request
+type EncryptDecryptData struct {
+	ID  string
+	Key string
+}
+
+// DecryptResponse defines the data sent back from the server on a retrieval request
+type DecryptResponse struct {
+	PlainText string
 }
 
 // Store packages up the data into a JSON structure and makes a call to the server
@@ -9,8 +43,34 @@ type HTTPClient struct {
 // that the data has been stored with, or an error if not able to store.
 func (c HTTPClient) Store(id, payload []byte) (aesKey []byte, err error) {
 	// Store the id and payload into a Request
+	encryptRequest := &EncryptRequest{
+		ID:        string(id),
+		PlainText: string(payload),
+	}
+	encryptRequestJSON, err := json.Marshal(encryptRequest)
+
+	requestURL := fmt.Sprintf("%s%s", c.ServerURL, storePath)
+
 	// Send the 'store' request to the server
-	return nil, nil
+	resp, err := http.Post(requestURL, "application/json", bytes.NewBuffer(encryptRequestJSON))
+	if err != nil {
+		return nil, err
+	}
+
+	// Read the data sent back, which should contain the encryption key
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var jsEncryptResponse EncryptDecryptData
+	err = json.Unmarshal(body, &jsEncryptResponse)
+	if err != nil {
+		return nil, err
+	}
+
+	return []byte(jsEncryptResponse.Key), nil
 }
 
 // Retrieve makes a request to the server to retrieve the stored (and encrypted) data.
@@ -18,9 +78,34 @@ func (c HTTPClient) Store(id, payload []byte) (aesKey []byte, err error) {
 // to encrypt the data.
 func (c HTTPClient) Retrieve(id, aesKey []byte) (payLoad []byte, err error) {
 	// Store the id and the aesKey into a Request
-	// Send the 'retrieve' request to the server
+	decryptRequest := &EncryptDecryptData{
+		ID:  string(id),
+		Key: string(aesKey),
+	}
+	decryptRequestJSON, err := json.Marshal(decryptRequest)
 
-	return nil, nil
+	requestURL := fmt.Sprintf("%s%s", c.ServerURL, retrievePath)
+
+	// Send the 'retrieve' request to the server
+	resp, err := http.Post(requestURL, "application/json", bytes.NewBuffer(decryptRequestJSON))
+	if err != nil {
+		return nil, err
+	}
+
+	// Read the data sent back, which should contain the encryption key
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var jsDecryptResponse DecryptResponse
+	err = json.Unmarshal(body, &jsDecryptResponse)
+	if err != nil {
+		return nil, err
+	}
+
+	return []byte(jsDecryptResponse.PlainText), nil
 }
 
 // NewHTTPClient uses the URL passed in to create a
@@ -30,12 +115,8 @@ func NewHTTPClient(serverURL string) *HTTPClient {
 	}
 }
 
-// func main() {
-// 	// Determine from values passed in whether we are storing or retrieving values
+func postRequest(URL string, jsonData io.Reader) (*http.Response, error) {
+	resp, err := http.Post(URL, "application/json", jsonData)
 
-// 	client := NewHTTPClient("http://localhost:8080")
-
-// 	// var client Client
-// 	// client := NewHTTPClient(..)
-// 	// client := NewGRPCClient(..)
-// }
+	return resp, err
+}
